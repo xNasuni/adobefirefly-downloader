5bc15f442f5f2eb9fa3e3844544fe2f651d430b0
xNasuni <62818119+xNasuni@users.noreply.github.com>
2023-06-15 19:12:54 -0400

    update as seen in universal

--- generative-fill.js
+++ generative-fill.js
@@ -1,4 +1,8 @@
-( async () => {
+(async () => {
+    if (document.getElementsByTagName('clio-inpaint').length == 0) {
+        return window.alert('You need to generate an image before you run this!')
+    }
+
     function GetCanvas() {
         return document.getElementsByTagName('clio-inpaint')[0].shadowRoot.firstElementChild.querySelector('clio-md-stage').shadowRoot.querySelector('clio-md-compositor-canvas').shadowRoot.firstElementChild
     }
@@ -6,16 +10,16 @@
     function DownloadCanvas(Canvas, FileName) {
         var DownloadElement = document.createElement('a')
         DownloadElement.setAttribute('download', FileName)
-        
+
         var ImageData = Canvas.toDataURL('image/png')
 
-        var URL = ImageData.replace(/^data:image\/png/,'data:application/octet-stream');
+        var URL = ImageData.replace(/^data:image\/png/, 'data:application/octet-stream');
 
         DownloadElement.setAttribute('href', URL)
         DownloadElement.click()
-        
+
         DownloadElement.remove()
     }
 
     DownloadCanvas(GetCanvas(), `firefly-${Date.now()}.jpg`)
-} )()
+})()
